8c6e1dfe0288ed25d9a3a9bcf2c6e8c257fb0f48
= <senechalsylvain@hotmail.com>
2021-04-05 04:12:20 +0200

    register from front

--- front/src/App.js
+++ front/src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
 
-function App() {
+const App = props => {
+  console.log(props)
+
+  const [pseudoRegister, setPseudoRegister] = useState("My Pseudo")
+  const [passwordRegister, setPasswordRegister] = useState("My Password")
+
+  const [isLogged, setIsLogged] = useState(false)
+
+  const handleSubmitRegistration = async event => {
+    event.preventDefault()
+    console.log('ok')
+    console.log(pseudoRegister)
+    console.log(passwordRegister)
+    let result = await fetch('http://localhost:8080/register', {
+      method: 'POST', // *GET, POST, PUT, DELETE, etc.
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({pseudo: pseudoRegister, password: passwordRegister})
+    })
+    let readableResult = await result.json()
+    console.log(readableResult)
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className="App">
+        <p> Register </p>
+        <form  onSubmit={handleSubmitRegistration}>
+          <label htmlFor="pseudo"> Enter your name: 
+            <input type="text" name="pseudo" id="pseudo" value={pseudoRegister} onChange={e => setPseudoRegister(e.target.value)} required />
+          </label>
+          <label htmlFor="password"> Enter your email:
+            <input type="password" name="password" id="password" value={passwordRegister} onChange={e => setPasswordRegister(e.target.value)} required />
+          </label>
+          
+          <input type="submit" value="Register"/>
+        </form>
+        <p> Login </p>
+      </div> 
+      {isLogged 
+        ? <CondComponent1 />
+        : <CondComponent2 />
+      }
+
+      
+      
+    </>
+  )
+}
+
+const CondComponent1 = props => {
+  return (
+    <>
+      <p> Je suis un GENTIL component conditionnel</p>
+    </>
+  )
+}
+
+const CondComponent2 = props => {
+
+  return (
+    <>
+      <p> Je suis un VILAIN component conditionnel</p>
+    </>
+  )
 }
 
 export default App;
