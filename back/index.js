d1517d1b86cd6f1a2dfecd97753d767e33bfdd15
= <senechalsylvain@hotmail.com>
2021-04-08 03:18:29 +0200

    backend handle connection

--- back/index.js
+++ back/index.js
@@ -1,86 +1,103 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 8080
+const KEY_JWT = "badObviousTestKey"
 // const dbPassword = process.env.DB_PASSWORD
 // const dbName = process.env.DB_NAME
 const jwt = require('jsonwebtoken')
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcrypt') // https://www.martinstoeckli.ch/hash/en/index.php
+const sqlite3 = require('better-sqlite3')
 
-console.log(port)
+// const database = new sqlite3(':memory:', { verbose: console.log })
+const databaseFile = new sqlite3('facebook.db', { verbose: console.log })
+app.locals.database = databaseFile
+
+const createDatabase = require('./dbCreation.js')
+
+createDatabase(databaseFile)
+
 app.use(express.json())
 app.listen(port)
+console.log('Listening on : ', port)
 
 app.use((req, res, next) => {
-    console.log(req.method)
 	res.header('Access-Control-Allow-Origin', '*')
 	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-type')
 	if (req.method === 'OPTIONS') {
-        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET')
-        return res.status(200).json({})
+		res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET')
+		return res.status(200).json({})
 	}
 	next()
 })
 
 const checkAuth = (req, res, next) => {
-    try {
-        const token = req.headers.authorization.split(" ")[1]
-        console.log(token)
-        const decoded = jwt.verify(token, KEY_JWT)
-        console.log(decoded)
-        req.userData = decoded
-        next()
-    } catch (error) {
-        console.log(error)
-        return res.status(401).json({message: 'auth failed'})
-    }
+	try {
+		const token = req.headers.authorization.split(" ")[1]
+		console.log(token)
+		const decoded = jwt.verify(token, KEY_JWT)
+		console.log(decoded)
+		req.userData = decoded
+		next()
+	} catch (error) {
+		console.log(error)
+		return res.status(401).json({ message: 'auth failed' })
+	}
 }
-
+// todo prepared statement fichier separe
+// todo	tester sql injections ' union SELECT name, password FROM users WHERE name <> '
+// tester différent work factor sur bcrypt
 app.post('/register', async (req, res) => {
-    console.log(req.body)
-    bcrypt.hash(req.body.password, 10, async (err, hashed) => {
-        if (err) {
-            return res.status(500).json({error: err})
-        } else {
-            const user = {
-                pseudo: req.body.pseudo,
-                password: hashed
-            }
-            // const db = req.app.locals.database
-            // await db.collection('Users').insertOne(user)
-            res.status(201).json({
-                message: 'user created'
-            })
-        }
-    })
+	console.log(req.body)
+	// verifier pseudo not exists
+	bcrypt.hash(req.body.password, 10, async (err, hashed) => {
+		if (err) {
+			return res.status(500).json({ error: err })
+		} else {
+			const database = req.app.locals.database
+			const statement = database.prepare("INSERT INTO user (pseudo, password) VALUES (?, ?)")
+			statement.run(req.body.pseudo, hashed)
+			const statement4 = database.prepare("SELECT user_id, rowid, * FROM user")
+			console.log(statement4.all())
+			res.status(201).json({
+				message: 'user created'
+			})
+		}
+	})
 })
 
-// app.post('/login', async (req, res) => {
-//     const db = req.app.locals.database
-//     const user = await db.collection('Users').findOne({email: req.body.email})
-//     if (!user) {
-//         return res.status(401).json({message: 'auth failed'})
-//     }
-//     console.log(user)
-//     bcrypt.compare(req.body.password, user.password, (err, result) => {
-//         if (err) {
-//             return res.status(401).json({message: 'auth failed'})
-//         } 
-//         if (result === true) {
-//             const token = jwt.sign({
-//                 email: user.email, 
-//                 userId: user._id
-//             },
-//             KEY_JWT,
-//             {
-//                 expiresIn: "1h"
-//             }
-//             )
-//             return res.status(200).json({message: 'auth successfull', token: token})
-//         } else {
-//             return res.status(401).json({message: 'auth failed'})
-//         }
-//     })
-// })
+app.post('/login', async (req, res) => {
+	const database = req.app.locals.database
+	console.log(req.body)
+	const statement = database.prepare("SELECT pseudo, password FROM user WHERE pseudo = ?")
+	const user = statement.get(req.body.pseudo)
+
+	console.log(user)
+
+	if (!user) {
+		return res.status(401).json({ message: 'auth failed' })
+	}
+	bcrypt.compare(req.body.password, user.password, (err, result) => {
+		console.log(err)
+		console.log(result)
+		if (err) {
+			return res.status(401).json({ message: 'auth failed' })
+		}
+		if (result === true) {
+			const token = jwt.sign({
+				email: user.email,
+				userId: user._id
+			},
+				KEY_JWT,
+				{
+					expiresIn: "1h"
+				}
+			)
+			return res.status(200).json({ message: 'successfull', token: token })
+		} else {
+			return res.status(401).json({ message: 'auth failed' })
+		}
+	})
+})
 
 app.use((req, res, next) => {
 	const error = new Error('Not found')
